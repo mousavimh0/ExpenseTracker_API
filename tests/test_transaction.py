27556d14b282240419f310fc3066c7d24acc7780
mmd <mousavijalal.mh@gmail.com>
2026-08-30 12:45:47 +0330

    test: add transaction pagination tests

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -412,3 +412,226 @@ def test_invalid_category(clean_database):
         headers={"Authorization": f"Bearer {ali_token}"},
     )
     assert ali_create_transaction_response.status_code == 422
+
+
+def test_pagination_limit_2(clean_database):
+    client.post(
+        "/users/create",
+        json={"username": "ali", "email": "ali@gmail.com", "password": "1"},
+    )
+
+    ali_login_response = client.post(
+        "/users/login", data={"username": "ali", "password": "1"}
+    )
+    ali_token = ali_login_response.json()["access_token"]
+
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 100,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 100,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 100,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 100,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 100,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+
+    response = client.get(
+        "/transactions/get?skip=0&limit=2",
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+
+    assert len(response.json()) == 2
+
+
+def test_pagination_skip_2(clean_database):
+    client.post(
+        "/users/create",
+        json={"username": "ali", "email": "ali@gmail.com", "password": "1"},
+    )
+
+    ali_login_response = client.post(
+        "/users/login", data={"username": "ali", "password": "1"}
+    )
+    ali_token = ali_login_response.json()["access_token"]
+
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 100,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 200,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 300,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 400,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 500,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+
+    response = client.get(
+        "/transactions/get?skip=2&limit=2",
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    amounts = [transaction["amount"] for transaction in response.json()]
+
+    assert len(response.json()) == 2
+    assert amounts == [300, 400]
+
+
+def test_pagination_user_can_only_see_all_owned_transactions(clean_database):
+
+    client.post(
+        "/users/create",
+        json={"username": "mmd", "email": "mmd@gmail.com", "password": "1"},
+    )
+    client.post(
+        "/users/create",
+        json={"username": "ali", "email": "ali@gmail.com", "password": "1"},
+    )
+
+    ali_login_response = client.post(
+        "/users/login", data={"username": "ali", "password": "1"}
+    )
+    ali_token = ali_login_response.json()["access_token"]
+
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 200,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 200,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 200,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {ali_token}"},
+    )
+
+    mmd_login_response = client.post(
+        "/users/login", data={"username": "mmd", "password": "1"}
+    )
+    mmd_token = mmd_login_response.json()["access_token"]
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 200,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {mmd_token}"},
+    )
+    client.post(
+        "/transactions/create",
+        json={
+            "type": "income",
+            "amount": 200,
+            "category": "salary",
+            "date_": "2026-08-26",
+        },
+        headers={"Authorization": f"Bearer {mmd_token}"},
+    )
+
+    response = client.get(
+        "/transactions/get?skip=0&limit=20",
+        headers={"Authorization": f"Bearer {mmd_token}"},
+    )
+    mmd_transactions: list = response.json()
+
+    assert len(mmd_transactions) == 2
